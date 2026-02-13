ec194639f09f3ebf3f6daffbd6d13ffee604750a
Roshan Patil <113788587+itsroshan09@users.noreply.github.com>
2026-02-13 13:36:27 +0530

    Create 'roshan' table in practical1.sql
    Added a new SQL table definition for 'roshan'.

--- app/src/main/practical1.sql
+++ app/src/main/practical1.sql
@@ -1,2 +1,46 @@
 
 create table roshan();
+
+
+    Directory: C:\Users\mayur
+
+
+-- Mode                 LastWriteTime         Length Name
+-- ----                 -------------         ------ ----
+-- da----        14-01-2026     22:50                .android
+-- d-----        29-01-2026     19:18                .arduinoIDE
+-- d-----        12-01-2026     23:01                .cache
+-- d-----        01-01-2026     16:43                .expo
+-- d-----        12-01-2026     23:01                .gradle
+-- d-----        31-01-2026     11:32                .ipython
+-- d-----        11-02-2026     12:43                .javacpp
+-- d-----        13-02-2026     10:44                .ssh
+-- d-----        02-01-2026     01:21                .vscode
+-- d-----        12-01-2026     23:39                AndroidStudioProjects
+-- d-----        24-12-2025     14:17                ansel
+-- d-----        12-01-2026     22:41                Chat App
+-- d-r---        05-12-2025     15:27                Contacts
+-- d-r---        05-02-2026     14:59                Desktop
+-- d-----        05-12-2025     16:23                Documents
+-- d-r---        13-02-2026     13:25                Downloads
+-- d-----        27-01-2026     02:58                Fake_News_Detection
+-- d-r---        05-12-2025     15:27                Favorites
+-- d-----        23-01-2026     13:42                flutter
+-- d-----        03-01-2026     16:33                intern-apps
+-- d-r---        05-12-2025     15:27                Links
+-- d-r---        05-12-2025     15:27                Music
+-- dar--l        13-01-2026     14:22                OneDrive
+-- d-r---        09-02-2026     13:32                Pictures
+-- d-----        03-01-2026     12:56                practice-app
+-- d-r---        05-12-2025     15:27                Saved Games
+-- d-r---        05-12-2025     15:33                Searches
+-- d-----        03-01-2026     14:18                shape-calci
+-- d-----        23-01-2026     13:02                Swasth_Jeevan
+-- d-----        01-01-2026     16:42                todoApp
+-- d-r---        11-02-2026     12:24                Videos
+-- -a----        13-02-2026     12:17           1091 .bash_history
+-- -a----        12-01-2026     23:56             16 .emulator_console_auth_token
+-- -a----        05-02-2026     00:38             53 .git-for-windows-updater
+-- -a----        25-01-2026     17:48             63 .gitconfig
+-- -a----        25-01-2026     17:49             20 .lesshst
+-- -a----        18-01-2026     05:40         115561 battery-report.html
